5e79a0921c24bb9f1f3f9b64ca6ab41d0cc3c14b
Arkadiusz <karas.tsukasa@gmail.com>
2020-12-09 22:25:49 +0100

    some debbuging

--- api-task/src/main/java/com/example/api/task/solution/services/AuthenticationService.java
+++ api-task/src/main/java/com/example/api/task/solution/services/AuthenticationService.java
@@ -1,12 +1,15 @@
 package com.example.api.task.solution.services;
 
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class AuthenticationService extends BaseService {
     private final String USER = System.getProperty("API_USER");
     private final String PASSWORD = System.getProperty("API_PASSWORD");
 
     public Response authenticate() {
+        log.info(USER);
         return getBaseSpec().auth().preemptive().basic(USER, PASSWORD).get("/authentication");
     }
 
